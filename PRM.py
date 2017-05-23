019ca83295870477cab18ce16ce71b6e2877cb05
dennismfong <dfong2014@gmail.com>
2017-05-23 10:10:13 -0700

    Add PRM.py via upload

---
+++ PRM.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+# Dennis Fong, Tim Kwong
+# 8234338, 8334492
+# 22 May 2017
+
+# ./PRM portNumber fileOfIPs
+
+import sys
+import socket
+
+LOCALHOST = '127.0.0.1'
+MYPORT = 5004
+LISTOFIPS = {}
+THELOG = {}
+
+# update the LISTOFIPS dict from config file
+def setupIPs():
+    with open(sys.argv[1], 'r') as configFile:
+        for line in configFile:
+            line = line.split()
+            LISTOFIPS[line[0]] = 1
+
+def leaderElection():
+    s
+
+def checkStream():
+    s
+
+# the main function
+setupIPs()
+servsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+servsock.bind((LOCALHOST, MYPORT))
+servsock.listen(10)
+print "Hi"
+servsock.close()
