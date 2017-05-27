9a96e0f4b845afcebb5ab959470099902a8c262a
dennismfong <dfong2014@gmail.com>
2017-05-26 19:45:02 -0700

    Pre C9, handling non main algorithm

--- prm.py
+++ prm.py
@@ -35,7 +35,10 @@ ACKDICT = {}
 IPDICT = {}
 
 # the nested 3d dictionary
-THELOG = {}
+THELOG = {} 
+
+#the current index of the log we are working on
+log_number = 0
 
 # string to socket
 # IP to socket
@@ -44,6 +47,7 @@ SOCKDICT = {}
 NUMACKS = 0
 NUMACCEPTS = 0
 ISLEADER = 0
+ISRUNNING = 1
 
 # update the LISTOFIPS dict from config file and MYIP
 def setupConfig():
@@ -64,60 +68,65 @@ def setupPorts():
 
 #  messages sent with spaces after each other, ballots separated by commas
 def checkStream():
-    global BALLOTNUM, PROPOSEDVAL, NUMACKS, NUMACCEPTS, ISLEADER, ACCEPTNUM, ACCEPTVAL
+    global BALLOTNUM, PROPOSEDVAL, NUMACKS, NUMACCEPTS, ISLEADER, ISRUNNING, ACCEPTNUM, ACCEPTVAL
     try:
         rawData = servsock.recv(1024)
         splitData = rawData.split()
         for ballot in splitData:
             ballotArgs = ballot.split(,)
-            if "replicate" in ballot:
-                # replicate,fileName
-                BALLOTNUM[0] = BALLOTNUM[0] + 1
-                PROPOSEDVAL = ballotArgs[1]
-                ISLEADER = 1
-                sendPrepare()
-            if "prepare" in ballot:
-                # prepare,ballot.num, ballot.ID
-                incomingNum = int(ballotArgs[1])
-                incomingID = int(ballotArgs[2])
-                incomingBallot = [incomingNum, incomingID]
-                if firstGreater(incomingBallot, BALLOTNUM):
-                    BALLOTNUM[0] = prepareNum
-                    BALLOTNUM[1] = prepareID
-                    sendAck(incomingBallot)
-            if "ack" in ballot:
-                # ack,proposedBal.num,proposedBal.ID,acceptBal.num,acceptBal.ID,acceptVal
-                NUMACKS = NUMACKS + 1
-                if NUMACKS == 1:
-                    incomingBal  = [int(ballotArgs[1]), int(ballotArgs[2])]
-                    incomingAcceptBal = [int(ballotArgs[3]), int(ballotArgs[4])]
-                    incomingVal = ballotArgs[5]
-                    if (incomingVal or ACCEPTVAL) is None:
-                        myTempVal = PROPOSEDVAL
-                    else:
-                        #does this work? only 3 nodes so maybe?
-                        if firstGreater(ACCEPTNUM, incomingAcceptBal):
+            if "stop" in ballot:
+                ISRUNNING = 0
+            if "resume" in ballot:
+                ISRUNNING = 1
+            if (ISRUNNING):
+                if "replicate" in ballot:
+                    # replicate,fileName
+                    BALLOTNUM[0] = BALLOTNUM[0] + 1
+                    PROPOSEDVAL = ballotArgs[1]
+                    ISLEADER = 1
+                    sendPrepare()
+                if "prepare" in ballot:
+                    # prepare,ballot.num, ballot.ID
+                    incomingNum = int(ballotArgs[1])
+                    incomingID = int(ballotArgs[2])
+                    incomingBallot = [incomingNum, incomingID]
+                    if firstGreater(incomingBallot, BALLOTNUM):
+                        BALLOTNUM[0] = prepareNum
+                        BALLOTNUM[1] = prepareID
+                        sendAck(incomingBallot)
+                if "ack" in ballot:
+                    # ack,proposedBal.num,proposedBal.ID,acceptBal.num,acceptBal.ID,acceptVal
+                    NUMACKS = NUMACKS + 1
+                    if NUMACKS == 1:
+                        incomingBal  = [int(ballotArgs[1]), int(ballotArgs[2])]
+                        incomingAcceptBal = [int(ballotArgs[3]), int(ballotArgs[4])]
+                        incomingVal = ballotArgs[5]
+                        if (incomingVal or ACCEPTVAL) is None:
                             myTempVal = PROPOSEDVAL
                         else:
-                            myTempVal = incomingVal
-                    leaderAccept(myTempVal)
-            if "accept" in ballot:
-                # accept,ballotNum.num,ballotNum.ID,myTempVal
-                NUMACCEPTS = NUMACCEPTS + 1
-                if NUMACCEPTS = 1:
-                    incomingBal = [int(ballotArgs[1]), int(ballotArgs[2])]
-                    incomingAcceptVal = ballotArgs[3]
-                    if firstGreater(incomingBal, BALLOTNUM):
-                        ACCEPTNUM[0] = incomingBal[0]
-                        ACCEPTNUM[1] = incomingBal[1]
-                        ACCEPTVAL = incomingAcceptVal
-                        cohortAccept(incomingBal, incomingAcceptVal)
-                    if (ISLEADER):
-                        decide()
-            if "decide" in ballot:
-                # decide,fileNameToReplicate
-                replicate(ACCEPTVAL)
-
+                            #does this work? only 3 nodes so maybe?
+                            if firstGreater(ACCEPTNUM, incomingAcceptBal):
+                                myTempVal = PROPOSEDVAL
+                            else:
+                                myTempVal = incomingVal
+                        leaderAccept(myTempVal)
+                if "accept" in ballot:
+                    # accept,ballotNum.num,ballotNum.ID,myTempVal
+                    NUMACCEPTS = NUMACCEPTS + 1
+                    if NUMACCEPTS = 1:
+                        incomingBal = [int(ballotArgs[1]), int(ballotArgs[2])]
+                        incomingAcceptVal = ballotArgs[3]
+                        if firstGreater(incomingBal, BALLOTNUM):
+                            ACCEPTNUM[0] = incomingBal[0]
+                            ACCEPTNUM[1] = incomingBal[1]
+                            ACCEPTVAL = incomingAcceptVal
+                            cohortAccept(incomingBal, incomingAcceptVal)
+                        if (ISLEADER):
+                            decide()
+                if "decide" in ballot:
+                    # decide,fileNameToReplicate
+                    replicate(ACCEPTVAL)
+            
 def firstGreater(ballot1, ballot2):
     if ballot1[0] > ballot2[0]:
         return true
@@ -150,11 +159,6 @@ def cohortAccept(b, v):
 def decide():
     for sock in SOCKDICT:
         SOCKDICT[sock].sendall("decide," + ACCEPTVAL)
-
-# prep the local log to send as a string to other PRMs
-def logToString(aLog):
-
-def stringToLog(aString)
                                
 # PRM FUNCTIONS #
 def merge(pos1, pos2):
@@ -185,16 +189,15 @@ def total(pos1, pos2):
     return total_count
 
 def print_log():
-    for index in THELOGS:
-        print THELOGS[index]['name']
+    for index in THELOG:
+        print THELOG[index]['name']
 
 
 def replicate(filename):
 ## placeholders for code referencing ##
-    THELOGS = {} 
-    THELOGS[log_number] = {}       ##log_number = whichever log the file is stored in order
-    THELOGS[log_number]['words'] = {}
-    THELOGS[log_number]['name'] = filename
+    THELOG[log_number] = {}       ##log_number = whichever log the file is stored in order
+    THELOG[log_number]['words'] = {}
+    THELOG[log_number]['name'] = filename
     
 ## end of placeholders ##
     readfile = open(filename, 'r')
@@ -204,36 +207,31 @@ def replicate(filename):
         else:
             word = line.split()[0]      #word
             wc = int(line.split()[1])   #word count
-            if word in THELOGS[filename]['words']:     #word already exists in logged dict
-                THELOGS[log_number]['words'][word] = THELOGS[filename]['words'][word] + wc 
+            if word in THELOG[filename]['words']:     #word already exists in logged dict
+                THELOG[log_number]['words'][word] = THELOG[filename]['words'][word] + wc 
             else:                                       #word doens't exist, add it
-                THELOGS[log_number]['words'][word] = wc
-    words = THELOGS[log_number]['words']        
-    rep_log = 'makenewlog,' + THELOGS[log_number]['name'] + '/'
-    for word in words
+                THELOG[log_number]['words'][word] = wc
+    words = THELOG[log_number]['words']        
+    rep_log = 'makenewlog,' + THELOG[log_number]['name'] + '/'
+    for word in words:
         rep_log = rep_log + word + '+' words[word] + '/' 
     #send rep_log to other PRMs to replicate
 
+    log_number = log_number + 1
+    
 def stringToLog(logString):
     words = logString.split('/')
     updatedName = False          #check if log has updated name
-    THELOGS[log_number] = {}
-    THELOGS[log_number]['words'] = {}
+    THELOG[log_number] = {}
+    THELOG[log_number]['words'] = {}
     for line in words:
         if not updatedName:
-            THELOGS[log_number]['name'] = line
+            THELOG[log_number]['name'] = line
             updatedName = True
         else:
             word = line.split('+')[0]
             wc = int(line.split('+')[1])
-            THELOGS[log_number]['words'][word] = wc
-    
-
-
-# store the file_reduced.txt into our local log
-def parseReduced(reducedFileName):
-                               
-                           
+            THELOG[log_number]['words'][word] = wc                      
 
 # the main function
 setupConfig()
